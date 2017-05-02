ad8b93a1c6b6ae65b88a4a17bf989ecfb2520d71
John Conley <JCC282@gmail.com>
2017-05-02 15:01:36 -0700

    Working with single keystrokes for multi-key commands

--- keyboard.cpp
+++ keyboard.cpp
@@ -4,12 +4,13 @@
 #define RESET_DELAY 110
 
 int chord = 0;
-uint8_t key = 0;
+// uint8_t key = 0;
 int alt = 0;
 boolean command = 0;
 boolean shift = 0;
 
 void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
   pinMode(0, INPUT_PULLUP);
   pinMode(1, INPUT_PULLUP);
   pinMode(2, INPUT_PULLUP);
@@ -22,46 +23,79 @@ boolean keyPressed(int n) {
 }
 
 boolean anyKeyPressed() {
-  return digitalRead(0) == LOW || digitalRead(1) == LOW || digitalRead(2) == LOW ||
-    digitalRead(3) == LOW || digitalRead(4) == LOW;
+  return keyPressed(0) || keyPressed(1) || keyPressed(2) || keyPressed(3) || keyPressed(4);
 }
 
-uint8_t alt0map[31] = {
-  0,               // nil
-  ' ',             // A
-  KEY_LEFT_GUI,    // B
-  KEY_DOWN_ARROW,  // AB
-  KEY_LEFT_SHIFT,  // C
-  KEY_BACKSPACE,   // AC
-  KEY_LEFT_ARROW,  // BC
-  's',             // ABC
-  0,               // D nil (alt)
-  '{',             // AD
-  'f',             // BD
-  '.',             // ABD
-  KEY_RIGHT_ARROW, // CD
-  '(',             // ACD
-  KEY_ESC,         // BCD
-  '[',             // ABCD
-  KEY_RETURN,      // E
-  '}',             // AE
-  'c',             // BE
-  't',             // ABE
-  'v',             // CE
-  0,               // ACE (nil)
-  0,               // BCE (nil for now)
-  0,               // ABCE (nil)
-  KEY_UP_ARROW,    // DE
-  ')',             // ADE
-  KEY_TAB,         // BDE
-  ']',             // ABDE
-  '_',             // CDE
-  0,               // ACDE (nil)
-  '='              // BCDE
+// {key_to_be_pressed, modifier_key_to_press_it_with}
+uint8_t alt0map[31][2] = {
+  {0, 0},               // nil
+  {' ', 0},             // A
+  {KEY_LEFT_GUI, 0},    // B
+  {KEY_UP_ARROW, 0},    // AB
+  {' ', KEY_LEFT_GUI},  // C
+  {KEY_BACKSPACE, 0},   // AC
+  {KEY_LEFT_ARROW, 0},  // BC
+  {'s', KEY_LEFT_GUI},             // ABC
+  {0, 0},               // D nil (alt)
+  {'{', KEY_LEFT_GUI},             // AD
+  {'f', KEY_LEFT_GUI},             // BD
+  {'.', 0},             // ABD
+  {KEY_RIGHT_ARROW, 0}, // CD
+  {'(', 0},             // ACD
+  {KEY_ESC, 0},         // BCD
+  {'[', KEY_LEFT_GUI},             // ABCD
+  {KEY_RETURN, 0},      // E
+  {'}', KEY_LEFT_GUI},             // AE
+  {'c', KEY_LEFT_SHIFT},             // BE
+  {'t', KEY_LEFT_GUI},             // ABE
+  {'v', KEY_LEFT_GUI},             // CE
+  {0, 0},               // ACE (nil)
+  {'F', KEY_LEFT_GUI},               // BCE (nil for now)
+  {0, 0},               // ABCE (nil)
+  {KEY_DOWN_ARROW, 0},  // DE
+  {')', 0},             // ADE
+  {KEY_TAB, KEY_LEFT_GUI},         // BDE
+  {']', KEY_LEFT_GUI},             // ABDE
+  {'_', 0},             // CDE
+  {0, 0},               // ACDE (nil)
+  {'=', 0}              // BCDE
 };
 
-// uint8_t[][3] altsMap = {};
-void pressKeyAndReset() {
+uint8_t alt1map[31][2] = {
+  {0, 0},               // nil
+  {'m', 0},             // A
+  {'r', 0},             // B
+  {'p', 0},             // AB
+  {'i', 0},             // C
+  {'b', 0},             // AC
+  {'l', 0},             // BC
+  {0, 0},             // ABC (nil for now)
+  {0, 0},               // D nil (alt)
+  {'d', 0},             // AD
+  {'h', 0},             // BD
+  {',', 0},             // ABD
+  {'k', 0},             // CD
+  {'a', 0},             // ACD
+  {'u', 0},             // BCD
+  {'x', 0},             // ABCD
+  {'n', 0},             // E
+  {'e', 0},             // AE
+  {'j', 0},             // BE
+  {'w', 0},             // ABE
+  {':', 0},             // CE
+  {0, 0},               // ACE (nil)
+  {0, 0},               // BCE (nil for now)
+  {0, 0},               // ABCE (nil)
+  {'g', 0},             // DE
+  {'o', 0},             // ADE
+  {'"', 0},             // BDE
+  {'y', 0},             // ABDE
+  {'-', 0},             // CDE
+  {0, 0},               // ACDE (nil)
+  {'z', 0}              // BCDE
+};
+
+void pressKey(uint8_t key) {
   if (key == KEY_LEFT_GUI) {
     command = !command;
     return;
@@ -76,22 +110,26 @@ void pressKeyAndReset() {
   if (command) Keyboard.press(KEY_LEFT_GUI);
 
   Keyboard.press(key);
+  delay(10);
   Keyboard.release(key);
 
   if (shift) Keyboard.release(KEY_LEFT_SHIFT);
   if (command) Keyboard.release(KEY_LEFT_GUI);
 
   // add logic
-  chord = 0;
-  alt = 0;
   command = 0;
   shift = 0;
 }
 
 void loop() {
+  uint8_t keys[2];
+  if (alt == 0) digitalWrite(LED_BUILTIN, HIGH);
+  if (alt == 1) digitalWrite(LED_BUILTIN, LOW);
   if (anyKeyPressed()) {
     delay(CHORD_DELAY);
 
+    chord = 0;
+    // key = 0;
     if (keyPressed(0)) chord = chord + 1;
     if (keyPressed(1)) chord = chord + 2;
     if (keyPressed(2)) chord = chord + 4;
@@ -99,13 +137,18 @@ void loop() {
     if (keyPressed(4)) chord = chord + 16;
 
     if (chord == 8) {
-      alt = (alt + 1) % 3;
+      alt = (alt + 1) % 2;
     } else {
-      key = alt0map[chord];
-      pressKeyAndReset();
+      if (alt == 0) { //keys = alt0map[chord];
+        if (alt0map[chord][1] != 0) pressKey(alt0map[chord][1]);
+        pressKey(alt0map[chord][0]);
+      }
+      if (alt == 1) { // key = alt1map[chord];
+        if (alt1map[chord][1] != 0) pressKey(alt1map[chord][1]);
+        pressKey(alt1map[chord][0]);
+      }
     }
 
     delay(RESET_DELAY);
   }
-
 }
